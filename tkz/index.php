f557c2e6a9b1793e77efd21d326ae9b5ec6edc06
Gustavo Trr <gustavotrr@gmail.com>
2016-11-22 13:17:04 -0200

    Finalizada Aula 2 do tkz! Criamos a paginação de posts e inserimos o link no título de cada post

--- tkz/index.php
+++ tkz/index.php
@@ -3,10 +3,17 @@
 	<div id="conteudo">
 		<h1>Conteúdo Geral</h1>
 		<?php if(have_posts()) : while (have_posts()) : the_post(); ?>
-		<h2><?php the_title(); ?></h2>
+		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		<p><?php the_content(); ?></p>
 		<hr>
-		<?php endwhile; else: ?>
+		<?php endwhile; ?>
+			<div class="paginacao">
+				<ul>
+					<li><?php next_posts_link('Posts Antigos'); ?></li>
+					<li><?php previous_posts_link('Novos posts'); ?></li>
+				</ul>
+			</div>
+		<?php else: ?>
 		<?php endif; ?>
 	</div>
 	<?php get_sidebar(); ?>
