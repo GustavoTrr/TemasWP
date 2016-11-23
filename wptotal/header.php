2c8e505d7f2fc986f345c58ca1d89ff49432c09b
Gustavo Trr <gustavotrr@gmail.com>
2016-11-23 11:53:35 -0200

    Dividido o conteúdo do template HTML em arquivos INDEX, HEADER, ARCHIVE, PAGE, SINGLE, SIDEBAR E FOOTER.

---
+++ wptotal/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "<a class="vglnk" href="http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd" rel="nofollow"><span>http</span><span>://</span><span>www</span><span>.</span><span>w3</span><span>.</span><span>org</span><span>/</span><span>TR</span><span>/</span><span>xhtml11</span><span>/</span><span>DTD</span><span>/</span><span>xhtml11</span><span>.</span><span>dtd</span></a>">
+<html xmlns="<a class="vglnk" href="http://www.w3.org/1999/xhtml" rel="nofollow"><span>http</span><span>://</span><span>www</span><span>.</span><span>w3</span><span>.</span><span>org</span><span>/</span><span>1999</span><span>/</span><span>xhtml</span></a>" xml:lang="en" xmlns:og="<a class="vglnk" href="http://opengraphprotocol.org/schema/" rel="nofollow"><span>http</span><span>://</span><span>opengraphprotocol</span><span>.</span><span>org</span><span>/</span><span>schema</span><span>/</span></a>" xmlns:fb="<a class="vglnk" href="http://www.facebook.com/2008/fbml" rel="nofollow"><span>http</span><span>://</span><span>www</span><span>.</span><span>facebook</span><span>.</span><span>com</span><span>/</span><span>2008</span><span>/</span><span>fbml</span></a>">
+<head>
+ 
+<title>Página de HTML</title>
+<link rel="stylesheet" href="style.css" type="text/css" />
+ 
+</head>
+<body>
+<div id="corpo">
+    <div id="header">
+        <h1>Titulo do Site</h1>
+        <h2>Descrição do site</h2>
+         
+        <ul id="nav">
+            <li><a href="#">Página 1</a></li>
+            <li><a href="#">Página 2</a></li>
+            <li><a href="#">Página 3</a></li>
+            <li><a href="#">Página 4</a></li>
+        </ul>
+    </div>
